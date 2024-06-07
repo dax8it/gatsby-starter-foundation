5d91a0e419ef8a05e3da0b3c9e4b7bfcb24c2b99
dax8it <alexcovo@gmail.com>
2024-06-07 16:56:10 -0400

    update test 2

---
+++ src/components/subdomains.js
@@ -0,0 +1,19 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout" // adjust path based on your project structure
+
+const SubdomainsPage = () => (
+  <Layout>
+    {/* Page content */}
+    <script
+      defer
+      id="namespace-widget"
+      widgetId="0x548c333a38cd063a93d94bf710466c0d7d9b9f31eec844c29296f948541e69b0"
+      type="module"
+      src="https://namespace.fra1.digitaloceanspaces.com/widget/latest/bundle.js">
+    </script>
+  </Layout>
+)
+
+export default SubdomainsPage
+
